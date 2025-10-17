4053ab3db39d161e83c9c572b0d5bc2ac0f95818
torresposso <erictorres.co@gmail.com>
2025-10-17 09:39:34 -0500

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import node from '@astrojs/node';
+import alpinejs from '@astrojs/alpinejs';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  adapter: node({
+    mode: 'standalone'
+  }),
+
+  integrations: [alpinejs()]
+});
